ed675f314e0f10a8c14dbe93d0a3cf9cd955b4dc
Nestal Wan <me@nestal.net>
2017-02-05 16:16:18 +0800

    promoted tree view

---
+++ gui/CodeBaseTreeView.hh
@@ -0,0 +1,25 @@
+/*
+	Copyright © 2017 Wan Wai Ho <me@nestal.net>
+    
+    This file is subject to the terms and conditions of the GNU General Public
+    License.  See the file COPYING in the main directory of the spaghetti
+    distribution for more details.
+*/
+
+//
+// Created by nestal on 2/5/17.
+//
+
+#pragma once
+
+#include <QTreeView>
+
+namespace gui {
+
+class CodeBaseTreeView : public QTreeView
+{
+public:
+	using QTreeView::QTreeView;
+};
+	
+} // end of namespace
