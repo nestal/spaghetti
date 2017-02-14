6de514209d319defe512132f7af3114bee91ac67
Nestal Wan <me@nestal.net>
2017-02-15 00:32:42 +0800

    (#10) calculate a smaller rectangle to redraw

--- gui/class_diagram/ClassItem.cc
+++ gui/class_diagram/ClassItem.cc
@@ -107,14 +107,22 @@ QVariant ClassItem::itemChange(QGraphicsItem::GraphicsItemChange change, const Q
 {
 	if (change == QGraphicsItem::ItemPositionChange)
 	{
+		auto affected = QRectF{pos(), value.toPointF()}.normalized();
+		affected.adjust(
+			0, 0, boundingRect().width(), boundingRect().height()
+		);
+		
 		for (auto&& edge : m_edges)
-			edge->update();
+		{
+//			edge->update();
+			affected |= edge->boundingRect();
+		}
 		
 		// redraw the whole scene
 		if (!m_edges.empty())
-			scene()->update();
+			scene()->update(affected);
 	}
-		
+
 	return value;
 }
 
